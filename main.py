cfcec75d1ded63726803c7cc4b6d84d850f00320
Kreton21 <dragulinescualexandru@tuta.io>
2022-03-29 08:32:06 +0200

    vbn

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-test
+import os as os
+import csv
+d=open("/test.csv")
+table=list(csv.DictReader(d,delimiter=","))
+
+
+
+f = open("index.txt", "w")
+f.writelines("test")
+f.close()
+
+
+fich = open("index.html", "w")
+fich.writelines(
+["<!DOCTYPE html>\n","<html>\n", "<head>\n","<title>\
+Table de multiplication </title>\n", "</head>\n",
+ "<body>\n","<h1> C'est le titre !</h1>","<ul>\n"])
+
+fich.writelines(genere_tableHTML(5))
+
+fich.writelines(["</ul>\n","</body>\n","</html>\n"])
+fich.close()
