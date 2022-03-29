712f121ed310814f341be699e7a3d7a9c254e1c1
Kreton21 <dragulinescualexandru@tuta.io>
2022-03-29 14:52:18 +0200

    light mode

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ jours=["Lundi","Mardi","Mercredi","Jeudi","Vendredi"]
 print(table)
 fich = open("index.html", "w")
 fich.writelines(
-["<!DOCTYPE html>\n","<html>\n", "<head>\n","<title >\
-Table de multiplication </title>\n", "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">", "</head>\n",
- "<body>\n","<h1> C'est le titre !</h1>","<ul>\n"])
+["<!DOCTYPE html>\n","<html>\n", "<head>\n","<title >Table de multiplication </title>\n",\
+"</head>\n",
+ "<body>\n","<h1> Reserver Tablettes !</h1>","<ul>\n"])
 
-fich.writelines("<div class=\"container\">")
-fich.writelines("<table class=\"table table-bordered \">")
+fich.writelines("<div>")
+fich.writelines("<table>")
 def initTab(tabl):
     fich.writelines("<tr>"+"<td>"+"Nom"+"</td>"+"<td>"+"Jour"+"</td>"+"<td>"+"Horaire"+"</td>"+"<td>"+"Durée"+"</td>"+"</tr>")
     for i in tabl:
@@ -25,8 +25,8 @@ def genereTab(tabl,i):
     fich.writelines("<td>"+str(i["Duree"])+"h</td>")
 def initTab2(tabl):
     global jours
-    fich.writelines("<div class=\"container\">")
-    fich.writelines("<table class=\"table table-bordered \">")
+    fich.writelines("<div>")
+    fich.writelines("<table>")
     fich.writelines("<tr>")
     fich.writelines("<td>"+"Heure"+"</td>")
     for i in range(5):
