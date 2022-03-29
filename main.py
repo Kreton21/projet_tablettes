29271e9026a19578473d36416066041fa84fbc99
Kreton21 <dragulinescualexandru@tuta.io>
2022-03-29 09:54:45 +0200

    First commit

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 import os as os
 import csv
-d=open("/test.csv")
+d=open("Test.csv")
 table=list(csv.DictReader(d,delimiter=","))
-
-
-
-f = open("index.txt", "w")
-f.writelines("test")
-f.close()
-
-
+jours=["Lundi","Mardi","Mercredi","Jeudi","Vendredi"]
+print(table)
 fich = open("index.html", "w")
 fich.writelines(
-["<!DOCTYPE html>\n","<html>\n", "<head>\n","<title>\
-Table de multiplication </title>\n", "</head>\n",
+["<!DOCTYPE html>\n","<html>\n", "<head>\n","<title >\
+Table de multiplication </title>\n", "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">", "</head>\n",
  "<body>\n","<h1> C'est le titre !</h1>","<ul>\n"])
 
-fich.writelines(genere_tableHTML(5))
+fich.writelines("<div class=\"container\">")
+fich.writelines("<table class=\"table table-bordered \">")
+def initTab(tabl):
+    fich.writelines("<tr>"+"<td>"+"Nom"+"</td>"+"<td>"+"Jour"+"</td>"+"<td>"+"Horaire"+"</td>"+"<td>"+"Durée"+"</td>"+"</tr>")
+    for i in tabl:
+        fich.writelines("<tr>")
+        genereTab(table,i)
+        fich.writelines("</tr>")
+def genereTab(tabl,i):
+    fich.writelines("<td>"+str(i["Nom"])+"</td>")
+    fich.writelines("<td>"+str(i["Jour"])+"</td>")
+    fich.writelines("<td>"+str(i["Horaire"])+"h</td>")
+    fich.writelines("<td>"+str(i["Duree"])+"h</td>")
+def initTab2(tabl):
+    global jours
+    fich.writelines("<div class=\"container\">")
+    fich.writelines("<table class=\"table table-bordered \">")
+    fich.writelines("<tr>")
+    fich.writelines("<td>"+"Heure"+"</td>")
+    for i in range(5):
+        fich.writelines("<td>"+jours[i]+"</td>")
+    fich.writelines("</tr>")
+    
+    for i in range (10):
+        fich.writelines("<tr>")
+        fich.writelines("<td>"+str(i+8)+"h</td>")
+        for i in range (5):
+            fich.writelines("<td>"+"</td>")
+        fich.writelines("</tr>")
+    fich.writelines("</table>")
+    fich.writelines("</div>")
+
+initTab(table)
+initTab2(table)
+fich.writelines("</div>")
+
+
 
 fich.writelines(["</ul>\n","</body>\n","</html>\n"])
 fich.close()
