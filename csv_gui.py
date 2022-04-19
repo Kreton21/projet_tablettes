c6fe0e1a5f53cd80b5b3b9e32404f96ff66e0457
Kreton21 <dragulinescualexandru@tuta.io>
2022-04-19 09:05:21 +0200

    vbn

--- csv_gui.py
+++ csv_gui.py
@@ -1,38 +1,18 @@
-from random import random
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.graphics import Color, Ellipse, Line
+from kivy.lang import Builder
+from kivy.app import MDApp
 
 
-class MyPaintWidget(Widget):
+class MainApp(MDApp):
+	def build(self):
+		self.theme_cls.theme_style = "Dark"
+		self.theme_cls.primary_palette = "BlueGray"
+		return Builder.load_file('csv_gui.kv')
+	def logger(self):
+		self.root.ids.welcome_label.text = f'Sup {self.root.ids.user.text}!'
 
-    def on_touch_down(self, touch):
-        color = (random(), 1, 1)
-        with self.canvas:
-            Color(*color, mode='hsv')
-            d = 30.
-            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
-            touch.ud['line'] = Line(points=(touch.x, touch.y))
-
-    def on_touch_move(self, touch):
-        touch.ud['line'].points += [touch.x, touch.y]
-
-
-class MyPaintApp(App):
-
-    def build(self):
-        parent = Widget()
-        self.painter = MyPaintWidget()
-        clearbtn = Button(text='Clear')
-        clearbtn.bind(on_release=self.clear_canvas)
-        parent.add_widget(self.painter)
-        parent.add_widget(clearbtn)
-        return parent
-
-    def clear_canvas(self, obj):
-        self.painter.canvas.clear()
-
-
-if __name__ == '__main__':
-    MyPaintApp().run()
+	def clear(self):
+		self.root.ids.welcome_label.text = "WELCOME"		
+		self.root.ids.user.text = ""		
+		self.root.ids.password.text = ""		
+	
+MainApp().run()
